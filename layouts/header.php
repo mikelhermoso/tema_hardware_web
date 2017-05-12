25370bf1217a1197b42f25ffab048f138362570f
mikel <mikel@creative>
2017-05-12 14:58:13 +0200

    12/05/2017 - cambios generales

---
+++ layouts/header.php
@@ -0,0 +1,53 @@
+<div id="cabecera">
+    <div id="cabeceraContent">
+        <div id="contentLogo">
+            <div id="containerLogo">
+                <a href="index.php?lang=es"><img class="imgLogo" src="img/logo.png" alt="español" /></a>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <a href="index.php"><li><?php echo $index ?></li></a>
+                <a href="#sobremi"><li><?php echo $sobremi ?></li></a>
+                <a href="#habs"><li><?php echo $habs ?></li></a>
+                <a href="blog.php"><li><?php echo $blog ?></li></a>
+                <a href="#contacto"><li><?php echo $contacto ?></li></a>
+            </ul>
+        </nav>
+        <div id="contentIdioma">
+            <a href="index.php?lang=es"><img class="imgIdioma" src="img/español.png" alt="español" /></a>
+            <a href="index.php?lang=en"><img class="imgIdioma" src="img/ingles.png" alt="ingles" /></a>
+        </div>
+        <div id="contentRedes">
+            <a href="https://twitter.com"><img class="imgRedes" src="img/redes/twitter.png" alt="twitter" /></a>
+            <a href="https://www.youtube.com"><img class="imgRedes" src="img/redes/youtube.png" alt="youtube" /></a>
+            <a href="https://www.linkedin.com"><img class="imgRedes" src="img/redes/linkedin.png" alt="linkedin" /></a>
+        </div>
+        <div id="contentHeaderText">
+            <h1 class="titHeader">TITULO EN PORTADA</h1>
+            <h4 class="textHeader">Texto en portada. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+            <h4 class="textHeader">Texto en portada. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+        </div>
+    </div>
+</div>
+<div id="cabeceraFix">
+    <nav>
+        <ul>
+            <a href="index.php"><li><?php echo $index ?></li></a>
+            <a href="#sobremi"><li><?php echo $sobremi ?></li></a>
+            <a href="#habs"><li><?php echo $habs ?></li></a>
+            <a href="blog.php"><li><?php echo $blog ?></li></a>
+            <a href="#contacto"><li><?php echo $contacto ?></li></a>
+        </ul>
+    </nav>
+    <div id="contentIdioma">
+        <a href="index.php?lang=es"><img class="imgIdioma" src="img/español.png" alt="español" /></a>
+        <a href="index.php?lang=en"><img class="imgIdioma" src="img/ingles.png" alt="ingles" /></a>
+    </div>
+    <div id="contentRedes">
+        <a href="https://twitter.com"><img class="imgRedes" src="img/redes/twitter.png" alt="twitter" /></a>
+        <a href="https://www.youtube.com"><img class="imgRedes" src="img/redes/youtube.png" alt="youtube" /></a>
+        <a href="https://www.linkedin.com"><img class="imgRedes" src="img/redes/linkedin.png" alt="linkedin" /></a>
+    </div>
+    <span class="ir-arriba"></span>
+</div>
---
+++ layouts/noticias.php
@@ -0,0 +1,10 @@
+<div id="contentNoticias">
+    <div id="containerNoticias">
+        <div id="contentTitNoticia">
+            <h2 class="titNoticia">TITULO NOTICIA</h2>
+        </div>
+        <div id="contentTextNoticia">
+            <p class="textoNoticia">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+        </div>
+    </div>
+</div>
